1ee940e2d6a11c341ae7fb30f11e6fc7331458d2
urmanta <urmanta@gmail.com>
2024-08-12 17:29:25 +0300

    Мессенджер, начало, README
    Мессенджер, начало, README
    
    Мессенджер, начало, README
    
    Мессенджер, начало, README
    
    Мессенджер, начало, README
    
    Мессенджер, начало, README

--- src/pages/chat/index.ts
+++ src/pages/chat/index.ts
@@ -4,8 +4,8 @@ export { default as ChatPage } from './chat-page.hbs?raw';
 
 Handlebars.registerHelper('chat-page-list', () => {
   return [
-    { name: 'Rainbow Dash', message: 'Дружба – это магия', unread: '2', avatar: "./assets/rainbowdash.jpg", date: ''},
-    { name: 'Applejack', message:'Отлично, сахарок!', avatar: "./assets/applejack.jpg"},
-    { name: 'Pinkie Pie', message:'О, я никогда не уезжаю из дома без своей пушки для праздника', unread: '4' , avatar: "./assets/pinkiepie.jpg"},
+    { name: 'Rainbow Dash', message: 'Дружба – это магия', unread: '2', date: ''},
+    { name: 'Applejack', message:'Отлично, сахарок!'},
+    { name: 'Pinkie Pie', message:'О, я никогда не уезжаю из дома без своей пушки для праздника', unread: '4'},
   ]
 });
